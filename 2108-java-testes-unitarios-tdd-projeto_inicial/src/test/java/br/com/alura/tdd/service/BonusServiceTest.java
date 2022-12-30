0c0c5e13f228eacacdc95a1dce8e0287c5612e5c
Rafael <rafaeldealmeida.192001@gmail.com>
2022-12-30 09:17:20 -0300

    Criando teste unitario para classe BonusService

---
+++ 2108-java-testes-unitarios-tdd-projeto_inicial/src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -0,0 +1,36 @@
+package br.com.alura.tdd.service;
+
+import br.com.alura.tdd.modelo.Funcionario;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BonusServiceTest {
+
+    @Test
+    void bonusDeveriaSerZeroParaFuncionarioComSalarioMuitoAlto() {
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("25000")));
+
+        assertEquals(BigDecimal.ZERO, bonus);
+    }
+
+    @Test
+    void bonusDeveriaSerDezPorCentoDoSalario() {
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("2500")));
+
+        assertEquals(new BigDecimal("250.0"), bonus);
+    }
+
+    @Test
+    void bonusDeveriaSerDezPorCentoParaSalarioDeExatamenteDezMil() {
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("10000")));
+
+        assertEquals(new BigDecimal("1000.0"), bonus);
+    }
+}
